45d0d4d6b18fdb747d1517d0ea5dba53db241d96
Lawrence, Andrew <andy.lawrence@gmail.com>
2017-10-16 17:57:33 -0400

    just use raw html snippet to test, not the full file

--- tests/tests.py
+++ tests/tests.py
@@ -6,9 +6,50 @@ sys.path.insert(0, ".")
 import utils
 
 class TestMainPage(unittest.TestCase):
-    # with self.assertRaises(TypeError):
     def setUp(self):
-        self.soup = utils.read_main_html('./AWS re_Invent 2017.htm')
+        raw_html = """
+            <div id="sessionsTab" aria-labelledby="ui-id-8" class="ui-tabs-panel ui-widget-content ui-corner-bottom" role="tabpanel" aria-expanded="true" aria-hidden="false">
+
+                    <div id="session_14490" class="resultRow sessionRow ">
+                        <div class="detailColumn">
+
+                            <a href="sessionDetail.ww?SESSION_ID=14490" class="openInPopup">
+
+                                <span class="abbreviation">ENT302 - </span>
+
+                                <span class="title">Optimizing Costs as You Scale on AWS</span>
+                            </a>
+
+                                <span class="abstract truncatedTxt">The cloud offers a first-in-a-career-opportunity to constantly optimize your costs as you grow and stay on the bleeding edge of innovation. By developing a cost-conscious... <a href="javascript:void(0);" class="moreLink truncated">View More</a></span>
+
+
+
+                                <small class="type">Breakout Session</small>
+
+
+                            <span class="scheduleStatus">
+
+
+
+
+                            </span>
+                        </div>
+                        <div class="actionColumn">
+
+                            <div class="sessionTimes">
+                                <a href="javascript:void(0);" class="expandSessionImg expanded"><span class="ww-icon ww-icon-carrot-e"></span>Scheduling Options</a>
+                                <ul class="availableSessions sessionTimeList" style="display: block;"><li><div class="tooltip">Session enrollment has not yet begun.<br></div>Monday, Nov 27, 10:00 AM - 11:00 AM<span class="sessionRoom"> – MGM, Level 1, Grand Ballroom 111</span></li></ul>
+                            </div>
+
+                            <a href="javascript:void(0);" class="interest interested"><span class="ww-icon ww-icon-star"></span>I'm no longer interested</a>
+
+
+                        </div>
+                    </div>
+    """
+
+
+        self.soup = BeautifulSoup(raw_html, 'html.parser')
         links = self.soup.findAll('a', attrs={'class':'openInPopup'})
         self.link = links[0]
 
